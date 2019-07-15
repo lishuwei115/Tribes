d4ab51f6da4e6703cbf0b2e6054cb1d7f2be7be0
LAPTOP-BBGJBUEQ\Asus <creative@playtra.com>
2019-07-15 17:50:46 +0100

    ACE - minimap

---
+++ Assets/CameraSizing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSizing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale =new Vector3 (Screen.width*(Camera.main.orthographicSize/WorldmapCamera.Instance.ZoomBounds[1]), Screen.height * (Camera.main.orthographicSize / WorldmapCamera.Instance.ZoomBounds[1]), 1);
+    }
+}
